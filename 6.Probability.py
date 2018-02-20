2efd58e0d4ec8914fb8e45dd57ed7b17bb049bac
74581 <W74581@gmail.com>
2018-02-20 20:39:45 +0800

    lesson 6.3

--- 6.Probability.py
+++ 6.Probability.py
@@ -4,8 +4,13 @@
 """a test mode"""
 
 import random
+import math
+from matplotlib import pyplot as plt
 
 __author__ = "74581"
+
+plt.rcParams['font.sans-serif'] = ['Simhei']  # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
 
 # 不独立和独立
@@ -57,3 +62,54 @@ def uniform_cdf(x):  # 累计分布函数(cumulative distribution function, cdf)
         return x  # e.g. P(X <= 0.4) = 0.4
     else:
         return 1  # 均匀分布的随机变量总是小于1
+
+
+# 正态分布
+
+def normal_pdf(x, mu=0, sigma=1.0):
+    sqrt_two_pi = math.sqrt(2 * math.pi)
+    return math.exp(-(x - mu) ** 2 / 2 / sigma ** 2) / (sqrt_two_pi * sigma)
+
+
+# xs = [x / 10.0 for x in range(-50, 50)]
+# plt.plot(xs, [normal_pdf(x, sigma=1) for x in xs], '-', label='mu=0,sigma=1')
+# plt.plot(xs, [normal_pdf(x, sigma=2) for x in xs], '--', label='mu=0,sigma=2')
+# plt.plot(xs, [normal_pdf(x, sigma=0.5) for x in xs], ':', label='mu=0,sigma=0.5')
+# plt.plot(xs, [normal_pdf(x, mu=-1) for x in xs], '-.', label='mu=-1,sigma=1')
+# plt.legend()
+# plt.title("多个正态分布的概率密度函数")
+# # plt.show()
+
+def normal_cdf(x, mu=0, sigma=1.0):
+    return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
+
+
+xs = [x / 10.0 for x in range(-50, 50)]
+plt.plot(xs, [normal_cdf(x, sigma=1) for x in xs], '-', label='mu=0,sigma=1')
+plt.plot(xs, [normal_cdf(x, sigma=2) for x in xs], '--', label='mu=0,sigma=2')
+plt.plot(xs, [normal_cdf(x, sigma=0.5) for x in xs], ':', label='mu=0,sigma=0.5')
+plt.plot(xs, [normal_cdf(x, mu=-1) for x in xs], '-.', label='mu=-1,sigma=1')
+plt.legend(loc=4)  # 底部右边
+plt.title("多个正态分布的累积分布函数")
+plt.show()
+
+
+def inverse_normal_cdf(p, mu=0, sigma=1, tolerance=0.00001):
+    """find approximate inverse using binary search"""
+    # 如果非标准型，先调整单位使之服从标准型
+    if mu != 0 or sigma != 1:
+        return mu + sigma * inverse_normal_cdf(p, tolerance=tolerance)
+    low_z, low_p = -10.0, 0  # normal_cdf(-10)是(非常接近)0
+    hi_z, hi_p = 10.0, 1  # normal_cdf(10)是(非常接近)1
+    while hi_z - low_z > tolerance:
+        mid_z = (low_z + hi_z) / 2  # 考虑中点
+        mid_p = normal_cdf(mid_z)  # 和cdf在那里的值
+        if mid_p < p:
+            # midpoint仍然太低，搜索比它大的值
+            low_z, low_p = mid_z, mid_p
+        elif mid_p > p:
+            # midpoint仍然太高，搜索比它小的值
+            hi_z, hi_p = mid_z, mid_p
+        else:
+            break
+    return mid_z
