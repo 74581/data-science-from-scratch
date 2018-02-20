463e726912656de211beaa4f662807db8f816425
74581 <W74581@gmail.com>
2018-02-20 21:06:37 +0800

    lesson 6.4

--- 6.Probability.py
+++ 6.Probability.py
@@ -5,6 +5,8 @@
 
 import random
 import math
+from collections import Counter
+
 from matplotlib import pyplot as plt
 
 __author__ = "74581"
@@ -84,14 +86,14 @@ def normal_cdf(x, mu=0, sigma=1.0):
     return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
 
 
-xs = [x / 10.0 for x in range(-50, 50)]
-plt.plot(xs, [normal_cdf(x, sigma=1) for x in xs], '-', label='mu=0,sigma=1')
-plt.plot(xs, [normal_cdf(x, sigma=2) for x in xs], '--', label='mu=0,sigma=2')
-plt.plot(xs, [normal_cdf(x, sigma=0.5) for x in xs], ':', label='mu=0,sigma=0.5')
-plt.plot(xs, [normal_cdf(x, mu=-1) for x in xs], '-.', label='mu=-1,sigma=1')
-plt.legend(loc=4)  # 底部右边
-plt.title("多个正态分布的累积分布函数")
-plt.show()
+# xs = [x / 10.0 for x in range(-50, 50)]
+# plt.plot(xs, [normal_cdf(x, sigma=1) for x in xs], '-', label='mu=0,sigma=1')
+# plt.plot(xs, [normal_cdf(x, sigma=2) for x in xs], '--', label='mu=0,sigma=2')
+# plt.plot(xs, [normal_cdf(x, sigma=0.5) for x in xs], ':', label='mu=0,sigma=0.5')
+# plt.plot(xs, [normal_cdf(x, mu=-1) for x in xs], '-.', label='mu=-1,sigma=1')
+# plt.legend(loc=4)  # 底部右边
+# plt.title("多个正态分布的累积分布函数")
+# plt.show()
 
 
 def inverse_normal_cdf(p, mu=0, sigma=1, tolerance=0.00001):
@@ -113,3 +115,38 @@ def inverse_normal_cdf(p, mu=0, sigma=1, tolerance=0.00001):
         else:
             break
     return mid_z
+
+
+# 中心极限定理
+
+def bernoulli_trial(p):
+    return 1 if random.random() < p else 0
+
+
+def binomial(n, p):
+    return sum(bernoulli_trial(p) for _ in range(n))
+
+
+def make_hist(p, n, num_points):
+    data = [binomial(n, p) for _ in range(num_points)]
+
+    # 用条形图绘出实际的二项式样本
+    histogram = Counter(data)
+    plt.bar([x - 0.4 for x in histogram.keys()],
+            [v / num_points for v in histogram.values()],
+            0.8,
+            color='0.75')
+
+    mu = p * n
+    sigma = math.sqrt(n * p * (1 - p))
+
+    # 用线型图绘出正态近似
+    xs = range(min(data), max(data) + 1)
+    ys = [normal_cdf(i + 0.5, mu, sigma) - normal_cdf(i - 0.5, mu, sigma)
+          for i in xs]
+    plt.plot(xs, ys)
+    plt.title("二项分布与正态近似")
+    plt.show()
+
+
+make_hist(0.75, 100, 10000)
