c7eee7b3781bd2ecff53e2471235d454f8a6a434
74581 <W74581@gmail.com>
2018-02-21 19:45:44 +0800

    Add lesson 8

---
+++ 8.Gradient_descent.py
@@ -0,0 +1,13 @@
+#!/user/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""a test mode"""
+
+__author__ = "74581"
+
+
+# 梯度下降的思想
+
+def sum_of_squares(v):
+    """computes the sum of squared elements in v"""
+    return sum(v_i ** 2 for v_i in v)
