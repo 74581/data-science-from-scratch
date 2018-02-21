5a030ec0e5db040e2571b2482301325861e1cfd6
74581 <W74581@gmail.com>
2018-02-21 16:53:54 +0800

    lesson 7.4

--- 7.Assumptions_and_inferences.py
+++ 7.Assumptions_and_inferences.py
@@ -170,5 +170,25 @@ experiments = [run_experiment() for _ in range(1000)]
 num_rejections = len([experiment
                       for experiment in experiments
                       if reject_fairness(experiment)])
-print(num_rejections)  # 46
-# p值不应该靠直觉得出，替代方案有下文的“贝叶斯推断”
+print(num_rejections)  # 46 p值不应该靠直觉得出，替代方案有下文的“贝叶斯推断”
+
+
+# 案例：运行A/B测试
+
+def estimated_parameters(N, n):
+    p = n / N
+    sigma = math.sqrt(p * (1 - p) / N)
+    return p, sigma
+
+
+def a_b_test_statistic(N_A, n_A, N_B, n_B):
+    p_A, sigma_A = estimated_parameters(N_A, n_A)
+    p_B, sigma_B = estimated_parameters(N_B, n_B)
+    return (p_B - p_A) / math.sqrt(sigma_A ** 2 + sigma_B ** 2)
+
+
+z = a_b_test_statistic(1000, 200, 1000, 180)  # -1.14
+two_sided_p_value(z)  # 0.254
+
+z = a_b_test_statistic(1000, 200, 1000, 150)  # -2.94
+two_sided_p_value(z)  # 0.003
