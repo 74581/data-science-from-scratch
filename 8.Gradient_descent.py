a551de062cb72f2673a8ed0fdeeb458781c272b9
74581 <W74581@gmail.com>
2018-02-22 20:40:59 +0800

    lesson 8.3

--- 8.Gradient_descent.py
+++ 8.Gradient_descent.py
@@ -3,7 +3,9 @@
 
 """a test mode"""
 
+import random
 from functools import partial
+import math
 from matplotlib import pyplot as plt
 
 __author__ = "74581"
@@ -35,20 +37,66 @@ def derivative(x):
 
 derivative_estimate = partial(difference_quotient, square, h=0.00001)
 
+
 # 绘出导入matplotlib.pyplot作为plt的基本相同的形态
-xs = range(-10, 10)
-plt.title("精确的导数值与估计值")
-"""
-map(derivative, xs)报错：
-AttributeError: 'numpy.ndarray' object has no attribute 'mask'
+# xs = range(-10, 10)
+# plt.title("精确的导数值与估计值")
+# """
+# map(derivative, xs)报错：
+# AttributeError: 'numpy.ndarray' object has no attribute 'mask'
+#
+# 原因：
+# map()在python3中返回generator而不是list
+#
+# 解决办法：
+# list(map())
+# """
+# plt.plot(xs, list(map(derivative, xs)), 'rx', label='Actual')
+# plt.plot(xs, list(map(derivative_estimate, xs)), 'b+', label='Estimate')
+# plt.legend(loc=9)
+# plt.show()
 
-原因：
-map()在python3中返回generator而不是list
 
-解决办法：
-list(map())
-"""
-plt.plot(xs, list(map(derivative, xs)), 'rx', label='Actual')
-plt.plot(xs, list(map(derivative_estimate, xs)), 'b+', label='Estimate')
-plt.legend(loc=9)
-plt.show()
+# f为多变量函数时，其他变量保持不便，计算第i个偏导数
+def partial_difference_quotient(f, v, i, h):
+    """compute the ith partial difference quotient of f at v"""
+    w = [v_j + (h if j == i else 0)  # 只对v的第i个元素增加h
+         for j, v_j in enumerate(v)]
+
+    return (f(w) - f(v)) / h
+
+
+# “差商估算法”的缺点是计算代价大
+def estimate_gradient(f, v, h=0.00001):
+    return [partial_difference_quotient(f, v, i, h)
+            for i, _ in enumerate(v)]
+
+
+# 使用梯度
+
+def step(v, direction, step_size):
+    """move step_size in the direction from v"""
+    return [v_i + step_size * direction_i
+            for v_i, direction_i in zip(v, direction)]
+
+
+def sum_of_squares_gradient(v):
+    return [2 * v_i for v_i in v]
+
+
+# 选取随机初始值
+v = [random.randint(-10, 10) for i in range(3)]
+
+tolerance = 0.0000001
+
+
+def distance(v, w):
+    return math.sqrt(sum((v_i - w_i) ** 2 for v_i, w_i in zip(v, w)))
+
+
+while True:
+    gradient = sum_of_squares_gradient(v)  # 计算v的梯度
+    next_v = step(v, gradient, -0.01)  # 取负的梯度步长
+    if distance(next_v, v) < tolerance:  # 如果收敛了就停止
+        break
+    v = next_v  # 如果没汇合就继续
