812a25fed3e4fa2d82c828990d8539e8cdc2d3fb
74581 <W74581@gmail.com>
2018-02-25 20:07:30 +0800

    lesson 9.3.1

--- 9.Retrieve_data.py
+++ 9.Retrieve_data.py
@@ -6,6 +6,8 @@
 import csv
 import re
 from collections import Counter
+import requests
+from bs4 import BeautifulSoup
 
 __author__ = '74581'
 
@@ -117,3 +119,42 @@ with open('comma_delimited_stock_prices.txt', 'wb') as f:
     for stock, price in today_prices.items():
         writer.writerow([stock, price])
 """
+
+# 网络抓取
+
+# HTML和解析方法
+# 为从HTML得到数据，使用BeautifulSoup库，它对网页元素建立树结构，提供获取接口
+# 也会用到requests库，比内置方法更好的发起HTTP请求的方式
+# 内置HTML解析器有点严格，需要用另一种解析器，html5lib库
+html = requests.get("https://www.baidu.com").text
+soup = BeautifulSoup(html, 'html5lib')
+
+# 第一个<p>标签（及其内容）
+first_paragraph = soup.find('p')  # 或仅仅soup.p
+
+# 使用text属性来得到文本内容
+first_paragraph_text = soup.p.text
+first_paragraph_words = soup.p.text.split()
+
+# 可以把标签当作字典提取属性
+first_paragraph_id = soup.p['id']  # 如果没有'id'则报出KeyError
+first_paragraph_id2 = soup.p.get('id')  # 如果没有'id'则返回None
+
+# 一次得到多个标签
+all_paragraphs = soup.find_all('p')  # 或仅仅soup('p')
+paragraphs_with_ids = [p for p in soup('p') if p.get('id')]
+
+# 通过类（class）来找标签
+important_paragraphs = soup('p', {'class': 'important'})
+important_paragraphs2 = soup('p', 'important')
+important_paragraphs3 = [p for p in soup('p')
+                         if 'important' in p.get('class', [])]
+
+# 将上述方法组合起来运用更复杂的逻辑
+# 比如找出包含在<div>元素中的每一个<span>元素
+# 警告，将多次返回同一个span元素
+# 如果它位于多个div元素里
+# 如果是这种情况，要更谨慎一些
+spans_inside_div = [span
+                    for div in soup('div')  # 对页面上的每一个<div>
+                    for span in div('span')]  # 找到其中的每一个<span>
